a12455a7ebadde8fe21cbc3ba1bc6d9d566dc497
Jaddison011 <ja3g20@soton.ac.uk>
2023-02-03 22:40:56 +0000

    Purged files, added basic sprites and movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,81 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
-    public GameObject ResumeButton;
-    public GameObject RestartButton;
-    public GameObject ExitButton;
-    public GameObject pauseCanvas;
-    public GameObject chargeBar;
-    public GameObject titleScreen;
-
-    public CheckpointManager checkpointManager;
+public class GameManager : MonoBehaviour {
 
     public Player player;
-    public Battery battery;
-
-    public AudioSource music;
-
-    private bool batteryOrNot;
-    private bool returnedToMenu = false;
     
     private void Awake() {
         Application.targetFrameRate = 60;
-        music.Play();
-        titleScreenMethod();
     }
+
 
     public void Pause() {
-        Time.timeScale = 0f;
-        player.enabled = false;
-        battery.enabled = false;
-
-        pauseCanvas.SetActive(true);
-        //ResumeButton.SetActive(true);
-        //RestartButton.SetActive(true);
-        //ExitButton.SetActive(true);
-        music.Pause();
-    }
-
-    public void GameOver() {
-        checkpointManager.Restart();
-        checkpointManager.reloadLevels();
-        checkpointManager.returnToCheckpoint();
-        music.Play();
-    }
-
-    public void ReturnToMenu() {
-        returnedToMenu = true;
-        titleScreenMethod();
-    }
-
-    public void titleScreenMethod() {
-        Time.timeScale = 0f;
-        player.enabled = false;
-        battery.enabled = false;
-        titleScreen.SetActive(true);
-        chargeBar.SetActive(false);
-    }
-
-    public void Play() {
-        battery.enabled = true;            
-        player.enabled = true;
-        Time.timeScale = 1f;
-        chargeBar.SetActive(true);
-        titleScreen.SetActive(false);
-        
-        //ResumeButton.SetActive(false);
-       // RestartButton.SetActive(false);
-       // ExitButton.SetActive(false);
-        pauseCanvas.SetActive(false);
-
-        Debug.Log(returnedToMenu);
-        if (returnedToMenu) {
-            checkpointManager.returnToCheckpoint();
-            checkpointManager.reloadLevels();
-        }
-        music.Play();
+        // Time.timeScale = 0f;
     }
 
     public void Exit() {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,79 +12,62 @@ using System.Collections;
      public float jumpHeight = 5;
      bool isGrounded = true;
 
-     public Animator animator;
+    //  public Animator animator;
      private SpriteRenderer spriteRenderer;
-     public bool robotEnabled = false;
 
-     private bool batteryNear = true;
-     public bool tutorial2Done = false;
 
-     public AudioSource walk1;
-     public AudioSource walk2;
-
-     public CheckpointManager checkpointManager;
+    //  public AudioSource walk1;
+    //  public AudioSource walk2;
 
      void FixedUpdate() {
+        //Feed moveDirection with input.
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        //Multiply it by speed.
+        moveDirection *= speed;
 
-        if (robotEnabled) {
-            //Feed moveDirection with input.
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            //Multiply it by speed.
-            moveDirection *= speed;
-    
-            //Making the character move
-            transform.position += moveDirection * Time.deltaTime;
-        }
+        //Making the character move
+        transform.position += moveDirection * Time.deltaTime;
      }
 
      private void Update() {
-        if (robotEnabled) {
-            animator.SetBool("Jumping", !isGrounded);
+        // animator.SetBool("Jumping", !isGrounded);
 
-            if(moveDirection.x != 0) {
-                animator.SetBool("Moving", true);
-
-
-                if (!walk2.isPlaying && !walk1.isPlaying) {
-                    walk1.Play();
-                    walk2.PlayDelayed(0.5f);
-                }
-            } else {
-                animator.SetBool("Moving", false);
-            }
-
-            if(Input.GetAxis("Horizontal") > 0) {
-                spriteRenderer.flipX = false;
-            } 
-            if(Input.GetAxis("Horizontal") < 0) {
-                spriteRenderer.flipX = true;
-            } 
-
-
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                
-                float jumpForce = Mathf.Sqrt(jumpHeight * jumpSpeed * (rb.gravityScale));
-
-                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Escape)) {
-                FindObjectOfType<GameManager>().Pause();
-            }
+        if(moveDirection.x != 0) {
+            // animator.SetBool("Moving", true);
+            // if (!walk2.isPlaying && !walk1.isPlaying) {
+            //     walk1.Play();
+            //     walk2.PlayDelayed(0.5f);
+            // }
+        } else {
+            // animator.SetBool("Moving", false);
         }
-    }
+
+        // if(Input.GetAxis("Horizontal") > 0) {
+        //     spriteRenderer.flipX = false;
+        // } 
+        // if(Input.GetAxis("Horizontal") < 0) {
+        //     spriteRenderer.flipX = true;
+        // } 
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            
+            float jumpForce = Mathf.Sqrt(jumpHeight * jumpSpeed * (rb.gravityScale));
+
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            FindObjectOfType<GameManager>().Pause();
+        }
+     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.GetType() == typeof(BoxCollider2D)) {
             // do stuff only for the box collider
             if(collision.gameObject.CompareTag("Floor")) {
                 isGrounded = true;
-            }
-            if(collision.gameObject.CompareTag("Lava")) {
-                checkpointManager.returnToCheckpoint();
-                checkpointManager.reloadLevels();
             }
         }
     }
@@ -99,34 +82,21 @@ using System.Collections;
     }
 
 
-    void OnTriggerEnter2D(Collider2D collider) {
-         // do stuff only for the circle collider
-        if(collider.gameObject.CompareTag("Battery")) {
-            Debug.Log("battery entered zone");
-            batteryNear = true;
-        }
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("enter");     
+        Debug.Log("Ground");
+        isGrounded = true;
     }
 
-    void OnTriggerExit2D(Collider2D collider) {
-         // do stuff only for the circle collider
-        if(collider.gameObject.CompareTag("Battery")) {
-            Debug.Log("battery left zone");
-            batteryNear = false;
-        }
+    void OnTriggerExit2D(Collider2D collision) {
+        Debug.Log("Exit");
+        Debug.Log("Air");
+        isGrounded = false;
     }
 
     private void Start() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator.SetBool("Jumping", false);
-        animator.SetBool("Moving", false);
-    }
-
-    public void SetRobotEnabled(bool enabled) {
-        robotEnabled = enabled;
-        animator.SetBool("Enabled", enabled);
-    }
-
-    public bool GetBatteryNear() {
-        return batteryNear;
+        // spriteRenderer = GetComponent<SpriteRenderer>();
+        // animator.SetBool("Jumping", false);
+        // animator.SetBool("Moving", false);
     }
  }
