bb6057663db41c2432e86012957c66bd4d2596ca
Jaddison011 <ja3g20@soton.ac.uk>
2023-02-04 20:33:11 +0000

    placed proper tiles

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,6 @@ public class CameraFollow : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 6f, -5);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3f, -5);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -68,21 +68,25 @@ using System.Collections;
             FindObjectOfType<GameManager>().Pause();
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        if (Input.GetMouseButtonDown(0)) {
             if (hoveredOver) {
                 onRoot = true;
                 pivot.GetComponent<Transform>().position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 root.enable(transform.position, pivot.GetComponent<Transform>().position);
                 root.SetPivot(pivot);
                 pivot.GetComponent<DistanceJoint2D>().enabled = true;
-            } else {
+            // } else {
+            //     onRoot = false;
+            //     root.disable();
+            //     pivot.GetComponent<DistanceJoint2D>().enabled = false;
+            // }
+            }
+        }
+        if (Input.GetMouseButtonUp(0)) {
                 onRoot = false;
                 root.disable();
                 pivot.GetComponent<DistanceJoint2D>().enabled = false;
-            }
-            
         }
-
      }
 
     void OnCollisionEnter2D(Collision2D collision) {
