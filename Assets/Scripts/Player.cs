afbb78709e8b6e8397fd287293204e59a55f6cb4
Jaddison011 <ja3g20@soton.ac.uk>
2023-02-05 11:09:47 +0000

    Player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,13 +12,14 @@ using System.Collections;
      public float jumpHeight = 5;
      bool isGrounded = true;
      bool onRoot = false;
+     bool jumping = false;
 
      public Root root;
      public GameObject pivot;
      public PlatformScript platforms;
      public bool hoveredOver = false;
 
-    //  public Animator animator;
+     public Animator animator;
      private SpriteRenderer spriteRenderer;
 
 
@@ -26,6 +27,7 @@ using System.Collections;
     //  public AudioSource walk2;
 
      void FixedUpdate() {
+        animator.SetBool("Jumping", jumping);
         //Feed moveDirection with input.
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         moveDirection = transform.TransformDirection(moveDirection);
@@ -37,28 +39,31 @@ using System.Collections;
      }
 
      private void Update() {
-        // animator.SetBool("Jumping", !isGrounded);
+        if (jumping) {
+            Debug.Log(jumping);
+        }
 
         if(moveDirection.x != 0) {
-            // animator.SetBool("Moving", true);
+            animator.SetBool("Moving", true);
             // if (!walk2.isPlaying && !walk1.isPlaying) {
             //     walk1.Play();
             //     walk2.PlayDelayed(0.5f);
             // }
         } else {
-            // animator.SetBool("Moving", false);
+            animator.SetBool("Moving", false);
         }
 
-        // if(Input.GetAxis("Horizontal") > 0) {
-        //     spriteRenderer.flipX = false;
-        // } 
-        // if(Input.GetAxis("Horizontal") < 0) {
-        //     spriteRenderer.flipX = true;
-        // } 
+        if(Input.GetAxis("Horizontal") > 0) {
+            spriteRenderer.flipX = false;
+        } 
+        if(Input.GetAxis("Horizontal") < 0) {
+            spriteRenderer.flipX = true;
+        } 
 
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            
+            jumping = true;
+            animator.SetBool("Jumping", true);
             float jumpForce = Mathf.Sqrt(jumpHeight * jumpSpeed * (rb.gravityScale));
 
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
@@ -75,11 +80,6 @@ using System.Collections;
                 root.enable(transform.position, pivot.GetComponent<Transform>().position);
                 root.SetPivot(pivot);
                 pivot.GetComponent<DistanceJoint2D>().enabled = true;
-            // } else {
-            //     onRoot = false;
-            //     root.disable();
-            //     pivot.GetComponent<DistanceJoint2D>().enabled = false;
-            // }
             }
         }
         if (Input.GetMouseButtonUp(0)) {
@@ -110,6 +110,8 @@ using System.Collections;
 
     void OnTriggerEnter2D(Collider2D collision) {
         isGrounded = true;
+        jumping = false;
+        animator.SetBool("Jumping", false);
     }
 
     void OnTriggerExit2D(Collider2D collision) {
@@ -117,9 +119,9 @@ using System.Collections;
     }
 
     private void Start() {
-        // spriteRenderer = GetComponent<SpriteRenderer>();
-        // animator.SetBool("Jumping", false);
-        // animator.SetBool("Moving", false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Moving", false);
     }
 
     public void SetHovering(bool hovering) {
